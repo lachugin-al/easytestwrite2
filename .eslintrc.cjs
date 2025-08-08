a3806f064528d1a8cf43bf3a7d777134d37f0c8d
lachugin-al <anton.lachugin.l@gmail.com>
2025-08-08 22:38:44 +0200

    project setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  env: {
+    es2022: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  ignorePatterns: ['dist/', 'node_modules/'],
+  rules: {},
+};
